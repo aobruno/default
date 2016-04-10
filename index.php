b7f6b4c1ba66cc79957b2766474cb630d486180d
Bruno Oliveira <brunooliveira@infojr.com.br>
2016-04-09 22:15:59 -0300

    versão desktop

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8"></head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <link rel="stylesheet" href="style.css">
+        <title>Default</title>
+    </head>
+    <body>
+        <header>
+            
+        </header>
+        <aside>
+            
+        </aside>
+        <section class="content">
+            <section class="wrapper">
+                
+            </section>
+        </section>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
